e197ae2133c8a9be76194efe868597114097f3d4
Alan_Garcia <warlock914@gmail.com>
2014-09-11 20:57:17 -0400

    Class3Lab5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,17 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [ :show, :edit, :update, :destroy ]
+  
   def index
   	@available_at = Time.now
 	@books = Book.all
   end
+  
   def show
     @book = Book.find(params[:id])
   end
 
   def new
-  @book = Book.new
+    @book = Book.new
   end
 
   def create
@@ -18,18 +21,28 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    #@book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
+    #@book = Book.find(params[:id])
     @book.update(book_params)
     redirect_to @book
+  end
+
+  def destroy
+    #@book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_url
   end
 
 private
 
   def book_params
     params.require(:book).permit(:title, :author, :pages, :price)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
