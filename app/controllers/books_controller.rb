70ed7fb1c32190405928d415a116d7a9cac42cd3
Alan_Garcia <warlock914@gmail.com>
2014-09-11 20:20:22 -0400

    Class3Lab4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,32 @@ class BooksController < ApplicationController
 	@books = Book.all
   end
   def show
-  @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+  redirect_to @book
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to @book
+  end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :pages, :price)
   end
 end
