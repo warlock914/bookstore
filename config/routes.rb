e6592fe887601dd9901d2b754c3fc6384fbcc77d
Alan Garcia <warlock914@gmail.com>
2014-09-25 20:23:25 -0400

    Class04Lab05

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Bookstore::Application.routes.draw do
   resources :books
+  root 'books#index'
   #get "/books" => "books#index", as: 'books'
   #get "/books/new" => "books#new", as: 'new_book'
   #get "/books/:id" => "books#show", as: 'book'
